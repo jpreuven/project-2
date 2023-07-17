c25985d621e17ce38005e3910845b30cb6a321a3
Kyle <kylekibbe99@gmail.com>
2023-07-17 16:15:11 -0700

    added discover page contents

--- src/CurrentTrending.js
+++ src/CurrentTrending.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CurrentTrending() {
-  return <div>CurrentTrending</div>;
+  const [trendingTracks, setTrendingTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchTrendingTracks = () => {
+      fetch(
+        "https://theaudiodb.com/api/v1/json/523532/trending.php?country=us&type=itunes&format=singles"
+      )
+        .then((response) => response.json())
+        .then((data) => setTrendingTracks(data.trending || []))
+        .catch((error) => {
+          console.error("Error fetching trending tracks:", error);
+        });
+    };
+
+    fetchTrendingTracks();
+  }, []);
+
+  return (
+    <div>
+      {trendingTracks.map((track) => (
+        <div key={track.idTrack}>
+          <h3>{track.strTrack}</h3>
+          <p>Artist: {track.strArtist}</p>
+          <p>Album: {track.strAlbum}</p>
+          <img
+            src={track.strTrackThumb || track.strArtistThumb}
+            alt={track.strTrack}
+            style={{ width: "200px", height: "200px" }}
+          />
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
